c2ac15e458c447ddcc69a2622b36705c92a67f1b
MalkySokol <57971058+MalkySokol@users.noreply.github.com>
2021-05-03 14:25:30 -0700

    updated main.js to include the logic of converting a number to a string

--- main.js
+++ main.js
@@ -1,12 +1,49 @@
 function convertToString(num){
 	//convert each number to a string representing the number
+	str = num;
+  	let string = "";
+  	for (var i = 0, len = str.length; i < len; i += 1) {
+		switch(+str.charAt(i)){
+		case 0:
+		  string += "Zero";
+		  break;
+		case 1:
+		  string += "One";
+		  break;
+		case 2:
+		  string += "Two";
+		  break;
+		case 3:
+		  string += "Three";
+		  break;
+		case 4:
+		  string += "Four";
+		  break;
+		case 5:
+		  string += "Five";
+		  break;
+		case 6:
+		  string += "Six";
+		  break;
+		case 7:
+		  string += "Seven";
+		  break;
+		case 8:
+		  string += "Eight";
+		  break;
+		case 9:
+		  string += "Nine";
+		  break;
+		}
+	}
+	process.stdout.write(string);
 }
 
 function parseNums() {
-  array = process.argv.slice(2); 
-  for (let index = 0; index < array.length; index++) {
-    convertToString(array[index]);
-  }
+	array = process.argv.slice(2); 
+	for (let index = 0; index < array.length; index++) {
+		convertToString(array[index]);
+  	}
 }
 
 parseNums();
